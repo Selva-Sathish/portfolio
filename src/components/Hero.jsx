362ace61777d42a1057e28926b2d1bada8bb46b8
selva satheesh <selvasathish@kenshinsystems.com>
2024-12-19 01:31:45 +0530

    Feat: Update content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { HERO_CONTENT } from '../constants/index'
-import profilePic from '../assets/KevinRushProfile.jpg'
+import { HERO_CONTENT } from '../constants/index';
+import profilePic from '../assets/hero.jpg';
+// import { motion } from 'framer-motion';
+
+
 const Hero = () => {
   return (
     <div className='border-b border-neutral-900 pb-4 lg:mb-36'>
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start'>
-                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
+                    <h1 
+                    className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
                         Selva Sathish
                     </h1>
                     <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
@@ -16,7 +19,7 @@ const Hero = () => {
             </div>
             <div className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex justify-center'>
-                    <img src={profilePic} alt="selva sathish" />
+                    <img src={profilePic} alt="selva sathish" height={500} width={500}/>
                 </div>
             </div>
         </div>
