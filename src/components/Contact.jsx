8305baafc0398e51e70e65aaf9d3b3df48bcfa64
selva satheesh <selvasathish@kenshinsystems.com>
2024-12-20 00:14:10 +0530

    add link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,8 @@ const Contact = () => {
         <h1 className='my-10 text-center text-4xl'>Get in Touch</h1>
         <div className='text-center tracking-tighter'>
             <p className='my-4'>{CONTACT.address}</p>
-            <p className='my-4'>{CONTACT.phoneNo}</p>
-            <a href={"mailto:"+CONTACT.email}>{CONTACT.email}</a>
+            <a className='my-4' href={"tel:+919789432788"}>{CONTACT.phoneNo}</a><br />
+            <a className='my-4' href={"mailto:"+CONTACT.email}>{CONTACT.email}</a>
         </div>
     </div>
   )
