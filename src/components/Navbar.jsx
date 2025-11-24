487bc6ad871df347205a4c89f61c29272e9b39f7
Selva-Sathish <selvasathishcse@gmail.com>
2025-11-24 21:19:31 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,12 @@ const Navbar = () => {
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
             <FaLinkedin />
-            <FaGithub />
+            <a
+              href="https://github.com/Selva-Sathish/"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
             <FaInstagram />
             <FaSquareXTwitter />
         </div>
